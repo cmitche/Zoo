cfdc8036b13f6e23354cdb477fd179e15f645a87
cmitche09 <cmitche09@gmail.com>
2020-12-18 08:49:09 -0500

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DietInformationComponent } from './components/diet-information/diet-information.component';
+import { HomeComponent } from './components/home/home.component';
+import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
 
 const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "gallery", component: ImageGalleryComponent},
+  {path: "diet", component: DietInformationComponent}
 ];
 
 @NgModule({
